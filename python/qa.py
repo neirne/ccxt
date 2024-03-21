fed3d2468880c0fd69b9ad138de0bdb7d0709af4
neirne <130891058+neirne@users.noreply.github.com>
2024-03-21 10:58:05 +0900

    Update qa.py

--- python/qa.py
+++ python/qa.py
@@ -15,3 +15,5 @@ else:
     command = 'flake8 --ignore=F722,F841,F821,W504,E402,E501,E275,E902 --exclude static_dependencies,node_modules,.tox,build'
     print(f'\n{command}\n')
     os.system(command)
+
+print('test')
